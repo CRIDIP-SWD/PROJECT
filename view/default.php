bf227b53a76e5ac259322d794a88a2e3341452fc
CRIDIP-SWD <mmockelyn@cridip.com>
2016-01-17 00:25:39 +0100

    PROJECT 1.0.0

--- view/default.php
+++ view/default.php
@@ -28,7 +28,7 @@
                 </ul>
                 <ul class="nav navbar-nav navbar-right">
                     <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><img src="" class="img-responsive img-circle" /> <span class="caret"></span></a>
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dashboard <span class="caret"></span></a>
                         <ul class="dropdown-menu">
                             <li><a href="#">Déconnexion</a></li>
                         </ul>
