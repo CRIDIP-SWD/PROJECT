84d6f29e90d5dcc77f1a0cc9409a99edc113f137
CRIDIP-SWD <mmockelyn@cridip.com>
2016-01-17 00:47:17 +0100

    PROJECT 1.0.0

--- view/default.php
+++ view/default.php
@@ -58,6 +58,14 @@
                     <li><a href="">One more nav</a></li>
                     <li><a href="">Another nav item</a></li>
                 </ul>
+                <?php if($view === 'dash_projet'): ?>
+                <ul class="nav nav-sidebar">
+                    <li>
+                        <i class="flaticon-chat98"></i>
+                        <span>TEST</span>
+                    </li>
+                </ul>
+                <?php endif; ?>
             </div>
             <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
                 <h1 class="page-header">Dashboard</h1>
